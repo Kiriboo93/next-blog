cee50ee08b446853b2346fd87a77f04b4ed1c36b
Kiriboo93 <robertoluquero@hotmail.com>
2025-04-14 15:38:44 +0200

    Formateado footer.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
     return (
-        <div className={styles.container}>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.links}>
+                <div className={styles.list}>
+                    <span className={styles.listTitle}>Links</span>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">Blog</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+                </div>
+                <div className={styles.list}>
+                    <span className={styles.listTitle}>Tags</span>
+                    <Link href="/">Hempidle</Link>
+                    <Link href="/">Dipets</Link>
+                    <Link href="/">Tales from Windy Meadow</Link>
+                </div>
+                <div className={styles.list}>
+                    <span className={styles.listTitle}>Links</span>
+                    <Link href="/">Facebook</Link>
+                    <Link href="/">Instagram</Link>
+                    <Link href="/">Tiktok</Link>
+                    <Link href="/">Youtube</Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
