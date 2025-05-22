35276151f97fe53c1470f9d5f84e1a482ff393fd
Kiriboo93 <robertoluquero@hotmail.com>
2025-05-22 18:25:24 +0200

    Implemented request to get categories from db.

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,25 +1,32 @@
-import React from "react";
 import styles from "./categoryList.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const CategoryList = () => {
+const getData = async () => {
+    const res = await fetch("http://localhost:3000/api/categories", {
+        cache: "no-store"
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed");
+    }
+
+    return res.json();
+}
+
+const CategoryList = async () => {
+    const data = await getData();
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Popular Categories</h1>
             <div className={styles.categories}>
-                <Link href="/blog?cat=hempidle" className={`${styles.category} ${styles.hempidle}`}>
-                    <Image src="/images/categories/style.png" alt="" width={75} height={50} className={styles.image} />
-                    <b className={styles.fixedText}>Hempidle</b>
-                </Link>
-                <Link href="/blog?cat=dipets" className={`${styles.category} ${styles.dipets}`}>
-                    <Image src="/images/categories/dipets.jpg" alt="" width={75} height={50} className={styles.image} />
-                    <b className={styles.fixedText}>Dipets</b>
-                </Link>
-                <Link href="/blog?cat=tales" className={`${styles.category} ${styles.tales}`}>
-                    <Image src="/images/categories/tales.jpg" alt="" width={75} height={50} className={styles.image} />
-                    <b className={styles.fixedText}>Tales from Windy Meadow</b>
-                </Link>
+                {data.categories?.map((item) => (
+                    <Link href={"/blog?cat=" + item.slug} className={`${styles.category} ${styles[item.slug]}`} key={item.id}>
+                        {item.img && (<Image src={"/images/categories" + item.img} alt={item.slug} width={75} height={50} className={styles.image} />)}
+                        <b className={styles.fixedText}>{item.title}</b>
+                    </Link>
+                ))}
             </div>
         </div>
     )
